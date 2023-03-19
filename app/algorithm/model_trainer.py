bfb4def51b6ea73cc86097806a9e7ee20996dd03
Abu Desai <abhyuday.desai@gmail.com>
2023-03-18 21:05:36 -0700

    cleanup

--- app/algorithm/model_trainer.py
+++ app/algorithm/model_trainer.py
@@ -24,7 +24,6 @@ def get_trained_model(train_data, data_schema, hyper_params):
     # preprocess data
     print("Pre-processing data...")
     train_X, _, preprocess_pipe = preprocess_data(train_data, None, data_schema)
-    # print('train_X shape:',  train_X.shape)  ; sys.exit()
 
     num_clusters = data_schema["datasetSpecs"]["suggestedNumClusters"]
 
@@ -37,27 +36,21 @@ def get_trained_model(train_data, data_schema, hyper_params):
 
 def train_model(train_X, hyper_params, num_clusters):
     # get model hyper-paameters parameters
-    # data_based_params = get_data_based_model_params(train_X)
     model_params = {**hyper_params, "K": num_clusters}
 
     # Create and train model
     model = Model(**model_params)
-    # model.summary()
     model.fit(train_X)
-    # print("last_loss:", history.history['loss'][-1])
     return model
 
 
 def preprocess_data(train_data, valid_data, data_schema):
     # print('Preprocessing train_data of shape...', train_data.shape)
     pp_params = pp_utils.get_preprocess_params(train_data, data_schema, model_cfg)
-    # pprint.pprint(pp_params)
 
     preprocess_pipe = pp_pipe.get_preprocess_pipeline(pp_params, model_cfg)
     train_data = preprocess_pipe.fit_transform(train_data)
-    # print("Processed train X/y data shape", train_data['X'].shape, train_data['y'].shape)
 
     if valid_data is not None:
         valid_data = preprocess_pipe.transform(valid_data)
-    # print("Processed valid X/y data shape", valid_data['X'].shape, valid_data['y'].shape)
     return train_data, valid_data, preprocess_pipe
